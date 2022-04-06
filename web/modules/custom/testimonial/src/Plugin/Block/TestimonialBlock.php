96b11ff264afceac123b458fdcaad965e753f2bf
Amy Helfman <amyh@users.noreply.github.com>
2022-04-06 03:09:53 -0400

    Add files and changes to testimonial module

--- web/modules/custom/testimonial/src/Plugin/Block/TestimonialBlock.php
+++ web/modules/custom/testimonial/src/Plugin/Block/TestimonialBlock.php
@@ -2,27 +2,45 @@
 
 namespace Drupal\testimonial\Plugin\Block;
 
-use Drupal\Core\Block\BlockBase;
-
 /**
- * Provides an example block.
+ * Provides a custom block type.
  *
  * @Block(
- *   id = "testimonial_example",
- *   admin_label = @Translation("Example"),
- *   category = @Translation("testimonial")
+ *  id = "testimonial",
+ *  admin_label = @Translation("Testimonial block"),
+ *  category = @Translation("Custom"),
+ *  deriver = "Drupal\block_content\Plugin\Derivative\BlockContent"
  * )
  */
-class TestimonialBlock extends BlockBase {
+class TestimonialBlock extends BlockContentBlock {
 
   /**
-   * {@inheritdoc}
+   * Overrides \Drupal\Core\Block\BlockBase::blockForm().
+   *
+   * Adds body and description fields to the block configuration form.
    */
-  public function build() {
-    $build['content'] = [
-      '#markup' => $this->t('It works!'),
+  public function blockForm($form, FormStateInterface $form_state) {
+    $block = $this
+      ->getEntity();
+    if (!$block) {
+      return $form;
+    }
+    $options = $this->entityDisplayRepository
+      ->getViewModeOptionsByBundle('block_content', $block
+        ->bundle());
+    $form['view_mode'] = [
+      '#type' => 'select',
+      '#options' => $options,
+      '#title' => $this
+        ->t('View mode'),
+      '#description' => $this
+        ->t('Output the block in this view mode.'),
+      '#default_value' => $this->configuration['view_mode'],
+      '#access' => count($options) > 1,
     ];
-    return $build;
+    $form['title']['#description'] = $this
+      ->t('The title of the block as shown to the user.');
+    return $form;
   }
 
 }
